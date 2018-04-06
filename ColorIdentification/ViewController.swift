9acd1d17aacc95808653c1c16dcf94fa344625e8
Admin <nareshjangid84@gmail.com>
2018-04-06 21:40:30 +0530

    added pods

---
+++ ColorIdentification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColorIdentification
+//
+//  Created by Admin on 06/04/18.
+//  Copyright © 2018 Naresh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
